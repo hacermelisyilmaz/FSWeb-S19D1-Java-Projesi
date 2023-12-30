23c5d0248b6ed4153b476ffe62c73cf5c34ca2d3
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-30 14:37:54 +0300

    Response records

---
+++ src/main/java/com/workintech/movie/dto/ActorResponse.java
@@ -0,0 +1,4 @@
+package com.workintech.movie.dto;
+
+public record ActorResponse(Long id, String firstName, String lastName, String birthDate) {
+}
---
+++ src/main/java/com/workintech/movie/dto/MovieResponse.java
@@ -0,0 +1,4 @@
+package com.workintech.movie.dto;
+
+public record MovieResponse(String name, String directorName) {
+}
